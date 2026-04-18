01ac565319acd0077b321f533ca09dfe9542ba65
NightMean <5726996+NightMean@users.noreply.github.com>
2026-04-19 01:02:47 +0200

    Restore Google Apache 2.0 headers on upstream-derived files and clean up .gitignore

--- Android/src/app/src/main/java/com/ollitert/llm/server/ui/server/logs/LogEntryCard.kt
+++ Android/src/app/src/main/java/com/ollitert/llm/server/ui/server/logs/LogEntryCard.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025-2026 @NightMean (https://github.com/NightMean)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ollitert.llm.server.ui.server.logs
 
 import com.ollitert.llm.server.ui.common.formatByteSize
@@ -69,7 +85,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
